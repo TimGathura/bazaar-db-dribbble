22d7e277a903d56068b2b2785b8195dd2dbcf0d6
TimGathura <timgathura@gmail.com>
2024-11-06 15:49:39 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/*/*.{css, js}",
+    "./src/*.html",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
